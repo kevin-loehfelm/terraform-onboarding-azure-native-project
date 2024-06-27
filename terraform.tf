d18851aea70e43d05a9b7ed71bf2cd8b9c9a96f2
kevin-loehfelm <kevin.loehfelm@hashicorp.com>
2024-06-27 17:24:24 -0400

    Comment out testing block

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-
+/*
   cloud {
     organization = "kloehfelm-demo"
     hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
@@ -8,7 +8,7 @@ terraform {
       name    = "test-demo-project"
     }
   }
-
+*/
   required_providers {
     azuread = {
       source = "hashicorp/azuread"
