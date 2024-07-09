889bef1b0e8157868ce98544f25df57eda2e02be
kevin-loehfelm <37027455+kevin-loehfelm@users.noreply.github.com>
2024-07-09 16:07:51 -0400

    Readme (#5)
    * test readme
    
    * readme test 2
    
    * Update readme 3
    
    * Update README
    
    * Update locals and readme

--- locals.tf
+++ locals.tf
@@ -3,8 +3,8 @@ locals {
   github_owner        = element(split("/", var.github_template_repo), 0)
   template_repository = element(split("/", var.github_template_repo), 1)
   workspaces = {
-    prod  = "prod-${var.project_name}"
-    dev   = "dev-${var.project_name}"
-    stage = "stage-${var.project_name}"
+    prod  = "${var.project_name}-prod"
+    dev   = "${var.project_name}-dev"
+    stage = "${var.project_name}-stage"
   }
 }
